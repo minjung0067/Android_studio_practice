5e81eae1c416e497721e00eb069f2d90a6c7b23a
minjung <minjung0067@naver.com>
2020-10-31 02:07:22 +0900

    'youtube#3'

--- app/src/main/java/com/example/practiveeee/MainActivity.java
+++ app/src/main/java/com/example/practiveeee/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.practiveeee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  //앱을 처음 실행할때 돌아가는 거
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et_test = findViewById(R.id.et_test);
+
+
+    //누르면 subactivity쪽으로 이동하는 버튼 만들기
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {   //버튼 눌렀을 때
+            @Override
+            public void onClick(View v) {  //이 함수를 실행시키겠다
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this , subactivity.class);
+                //위 subactivity는 내가 버튼을 만든 곳 자바파일의 이름
+                intent.putExtra("str", str);
+                startActivity(intent); //실제적으로 액티비티 이동하는 구문
+
+           }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/practiveeee/subactivity.java
+++ app/src/main/java/com/example/practiveeee/subactivity.java
@@ -1,14 +1,26 @@
 package com.example.practiveeee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class subactivity extends AppCompatActivity {
+
+    private TextView tv_sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subactivity);
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent();//날아오는 데이터 값 받겠다
+        String str = intent.getStringExtra("str");
+
+
+
+        tv_sub.setText(str);
     }
 }
